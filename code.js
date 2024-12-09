8075277027091566f651ed44a8119029f820b69a
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-09 14:00:01 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,31 @@
 function tsp_hk(distance_matrix) {
     const n = distance_matrix.length;
+    let newMin = Infinity;
+    let cityList = {};
     if ((n == 0) || (n == 1)) {
         return 0;
     }
     const mem = Array.from({ length: n }, () => ({}));
+    for (let i = 0; i < size; i++) {
+        let cities = [];
 
-    function heldKarp(city, cities, matrix, citylist) {
+        // Fill array with the all cities
+        for (let j = 0; j < n; j++) { 
+            cities.push(j); 
+        }
+
+        let min = heldKarpEq(i, cities, distance_matrix, cityList);
+        if (min < newMin) {
+            newMin = min; 
+        }
+    }
+    return newMin;
+}
+
+    function heldKarpEq(city, cities, matrix, cityList) {
     let cityNum = cities.length;
     let key = JSON.stringify([city, cities]);
-    if (citylist[key] != undefined) {
+    if (cityList[key] != undefined) {
         return cityList[key]; 
     }
     if (cityNum == 2) {
@@ -31,7 +48,7 @@ function tsp_hk(distance_matrix) {
                 }
                 newCities.push(cities[j]);
             }
-            let dis = matrix[city][cities[i]] + heldKarp(cities[i], newCities, matrix, cityList);
+            let dis = matrix[city][cities[i]] + heldKarpEq(cities[i], newCities, matrix, cityList);
             if (minDistance > distance) { 
                 minDistance = distance; 
             }
@@ -39,5 +56,4 @@ function tsp_hk(distance_matrix) {
         cityList[key] = minDistance;
         return minDistance;
     }
-}
 }
